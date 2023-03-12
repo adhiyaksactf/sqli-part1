5ee439fec4bfb463a889ef2ada6450cf89625990
Evan-aja <gebel909@student.ub.ac.id>
2023-03-12 22:03:21 +0700

    blocked 3306 and cleanup

--- app/config.php
+++ app/config.php
@@ -3,4 +3,4 @@ error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 $conn = mysqli_connect("db", "sqli", "sqli");
-mysqli_select_db($conn, 'sqli-login');
+mysqli_select_db($conn, 'sqli');
