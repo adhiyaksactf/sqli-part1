3166f87e1e47d27e0df2d34da3968ac76fa8b42d
Adhiyaksa <96332663+adhiyaksactf@users.noreply.github.com>
2023-03-12 15:46:49 +0700

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,6 +1,7 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
-$conn = mysqli_connect("127.0.0.1", "root", "");
-mysqli_select_db($conn, 'sqli-login');
+    error_reporting(E_ALL);
+    ini_set('display_errors', TRUE);
+    ini_set('display_startup_errors', TRUE);
+    $conn = mysqli_connect("localhost","root","");
+    mysqli_select_db($conn, 'sqli-login');
+?>
