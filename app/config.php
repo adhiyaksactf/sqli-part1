065c0f232bedfd96a5f376eb37e038983170f2c4
Dimas Tri Mustakim <tri.dimas@student.ub.ac.id>
2023-03-12 14:56:29 +0700

    remove networks

--- app/config.php
+++ app/config.php
@@ -2,5 +2,5 @@
 error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
-$conn = mysqli_connect("127.0.0.1", "root", "");
+$conn = mysqli_connect("db", "sqli", "sqli");
 mysqli_select_db($conn, 'sqli-login');
